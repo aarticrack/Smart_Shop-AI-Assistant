39e9307496a932e21ae58058a38dd0cbd9f8c31e
Aarti Kumari <aartiboss.007@gmail.com>
2026-04-07 18:37:33 +0530

    Engine of the Smart_Shop_AI

--- src/data_layer/ingest.py
+++ src/data_layer/ingest.py
@@ -3,7 +3,7 @@ from langchain_core.documents import Document
 from src.core.config import settings
 from src.data_layer.vector_store import VectorStoreManager
 from langchain_community.vectorstores import Chroma
-from src.utils.logger import logger  # Ensuring the project logger is used
+from src.utils.logger import logger  
 
 
 def parse_specs(specs_text):
@@ -21,7 +21,7 @@ def run_ingestion():
     logger.info(f" Starting Data Ingestion from {settings.DATA_FILE}")
     
     try:
-        # Load CSV and clean up column names (handles 'name' vs 'product_name')
+        # Loading CSV and clean up column names (handles 'name' vs 'product_name')
         df = pd.read_csv(settings.DATA_FILE)
         df.columns = df.columns.str.strip() 
         
@@ -32,9 +32,10 @@ def run_ingestion():
             # COMBINE EVERY COLUMN: This ensures Name, Price, and Description are all searchable
             searchable_text = " ".join([f"{col}: {val}" for col, val in row.items()])
             
-            # MAP METADATA: Used for the 'stock_status' filter in vector_store.py
+            # specs
 
             specs_data=parse_specs(row.get('specs',""))
+            #metadata from the dataset
             metadata = {
 
                 
@@ -73,5 +74,3 @@ def run_ingestion():
     except Exception as e:
         logger.error(f"ERROR during ingestion: {str(e)}")
 
-if __name__ == "__main__":
-    run_ingestion()
